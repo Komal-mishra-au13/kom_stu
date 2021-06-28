27040a0abad230cf154d592ab1ac53a6be7aa631
komal <komalmishra.bca@gmail.com>
2021-06-28 20:56:31 +0530

    projectcode added

---
+++ myPro/backend/server.js
@@ -0,0 +1,40 @@
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+import path from "path";
+
+dotenv.config();
+
+connectDB();
+
+const app = express(); // main
+
+app.use(express.json()); // to accept json data
+
+app.use("/api/user", userRoutes);
+
+const __dirname = path.resolve();
+app.use("/uploads", express(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build"))),
+    app.get("*", (req, res) =>
+      res.sendFile(path.resolve(__dirname, "frontend", "build", "index,html"))
+    );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}..`.yellow
+      .bold
+  )
+);
